52669e7f2cbd9684f8b160a591692459cf272d69
justyummy <827176083@qq.com>
2018-11-06 17:24:22 +0800

    Site updated: 2018-11-06 17:24:22

--- js/main.js
+++ js/main.js
@@ -10,6 +10,83 @@ if (!!$.prototype.justifiedGallery) {
   $(".article-gallery").justifiedGallery(options);
 }
 
+function addDarkTheme() {
+  var link = document.createElement('link');
+  link.type = 'text/css';
+  link.id = "theme-dark";  
+  link.rel = 'stylesheet';
+  link.href = '/css/style2.css';
+  document.getElementsByTagName("head")[0].appendChild(link);
+}
+
+function removeDarkTheme() {
+  $('#theme-dark').remove();
+}
+
+function sclick() {
+  // alert("Can not change theme");
+  // alert(cout)
+  // alert(getThemeCSSName() == 'dark')
+  if (getThemeCSSName() == 'light') {
+    addDarkTheme()
+    setCookie('theme', 'dark')
+  }else{
+    removeDarkTheme()
+    setCookie('theme', 'light')
+  }
+}
+
+function getThemeCSSName() {
+  return getCookie('theme') || "light";
+}
+
+function useDarkTheme(useDark) {
+  if (useDark) {
+      addDarkTheme();
+      setCookie('theme', 'dark')
+  } 
+}
+
+$(document).ready(function () {
+  useDarkTheme(getThemeCSSName() == 'dark');
+})
+
+
+function setCookie ( name, value )
+{
+    var expdate = new Date();
+    let expdays = 7
+    //设置Cookie过期日期
+    expdate.setDate(expdate.getDate() + expdays) ;
+    //添加Cookie
+    // console.log(name + "=" + escape(value) + ";path=/;expires=" + expdate.toUTCString())
+    document.cookie = name + "=" + escape(value) + ";path=/;expires=" + expdate.toUTCString();
+}
+
+function getCookie ( name )
+{
+    //获取name在Cookie中起止位置
+    var start = document.cookie.indexOf(name+"=") ;
+    console.log(document.cookie)
+    if ( start != -1 )
+    {
+        start = start + name.length + 1 ;
+        //获取value的终止位置
+        var end = document.cookie.indexOf(";", start) ;
+        if ( end == -1 )
+            end = document.cookie.length ;
+        //截获cookie的value值,并返回
+        console.log(document.cookie.substring(start,end))
+        return unescape(document.cookie.substring(start,end)) ;
+    }
+    return "" ;
+}
+
+function delCookie ( name )
+{
+    setCookie ( name, "", -1 ) ;
+}
+
 $(document).ready(function() {
 
   /**
@@ -108,6 +185,6 @@ $(document).ready(function() {
           $("#actions-footer > #top").show();
         }
       });
-    }
+    } 
   }
 });
