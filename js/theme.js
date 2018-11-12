f4256277d358380f6c664f6f5318dcc64576e1e0
justyummy <827176083@qq.com>
2018-11-12 10:56:06 +0800

    Site updated: 2018-11-12 10:56:05

--- js/theme.js
+++ js/theme.js
@@ -3,7 +3,7 @@
 function addDarkTheme() {
   var link = document.createElement('link');
   link.type = 'text/css';
-  link.id = "theme-dark";  
+  link.id = "theme-dark";
   link.rel = 'stylesheet';
   link.href = '/css/style2.css';
   document.getElementsByTagName("head")[0].appendChild(link);
@@ -17,8 +17,9 @@ function sclick() {
   // alert("Can not change theme");
   if (getThemeCSSName() == 'light') {
     addDarkTheme()
+    addDarkThemeColor();
     setCookie('theme', 'dark')
-  }else{
+  } else {
     removeDarkTheme()
     setCookie('theme', 'light')
   }
@@ -30,47 +31,51 @@ function getThemeCSSName() {
 
 function useDarkTheme(useDark) {
   if (useDark) {
-      addDarkTheme();
-      setCookie('theme', 'dark')
-  } 
+    addDarkTheme();
+    addDarkThemeColor();
+    setCookie('theme', 'dark')
+  }
 }
 
 $(document).ready(function () {
   useDarkTheme(getThemeCSSName() == 'dark');
 })
 
-
-function setCookie ( name, value )
-{
-    var expdate = new Date();
-    let expdays = 7
-    //设置Cookie过期日期
-    expdate.setDate(expdate.getDate() + expdays) ;
-    //添加Cookie
-    // console.log(name + "=" + escape(value) + ";path=/;expires=" + expdate.toUTCString())
-    document.cookie = name + "=" + escape(value) + ";path=/;expires=" + expdate.toUTCString();
+function addDarkThemeColor() {    //添加Chrome状态栏颜色
+  var meta = document.createElement('meta');
+  meta.name = 'theme-color';
+  meta.content = '#1d1f21'
+  document.getElementsByTagName("head")[0].appendChild(meta);
 }
 
-function getCookie ( name )
-{
-    //获取name在Cookie中起止位置
-    var start = document.cookie.indexOf(name+"=") ;
-    console.log(document.cookie)
-    if ( start != -1 )
-    {
-        start = start + name.length + 1 ;
-        //获取value的终止位置
-        var end = document.cookie.indexOf(";", start) ;
-        if ( end == -1 )
-            end = document.cookie.length ;
-        //截获cookie的value值,并返回
-        console.log(document.cookie.substring(start,end))
-        return unescape(document.cookie.substring(start,end)) ;
-    }
-    return "" ;
+function setCookie(name, value) {
+  var expdate = new Date();
+  let expdays = 7
+  //设置Cookie过期日期
+  expdate.setDate(expdate.getDate() + expdays);
+  //添加Cookie
+  // console.log(name + "=" + escape(value) + ";path=/;expires=" + expdate.toUTCString())
+  document.cookie = name + "=" + escape(value) + ";path=/;expires=" + expdate.toUTCString();
 }
 
-function delCookie ( name )
-{
-    setCookie ( name, "", -1 ) ;
+function getCookie(name) {
+  //获取name在Cookie中起止位置
+  var start = document.cookie.indexOf(name + "=");
+  console.log(document.cookie)
+  if (start != -1) {
+    start = start + name.length + 1;
+    //获取value的终止位置
+    var end = document.cookie.indexOf(";", start);
+    if (end == -1)
+      end = document.cookie.length;
+    //截获cookie的value值,并返回
+    console.log(document.cookie.substring(start, end))
+    return unescape(document.cookie.substring(start, end));
+  }
+  return "";
 }
+
+function delCookie(name) {
+  setCookie(name, "", -1);
+}
+
